91cbbc7a2a8ea5d6d003596421607a7cf9f48d0c
Elchanan Shuky Shukrun <elnn.sh@gmail.com>
2019-12-20 18:57:42 +0200

    add login package

---
+++ src/client/login/LoginUIController.java
@@ -0,0 +1,29 @@
+package client.login;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginUIController {
+
+    @FXML
+    private TextField userName;
+
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+    void initialize() {
+
+    }
+
+}
