c0ee2107431178e1df3fa780087e011b7081e6d0
Elchanan Shuky Shukrun <elnn.sh@gmail.com>
2019-12-25 15:09:17 +0200

    fix InitiatorButtons.java bug - loading icon

--- src/client/crDetails/initiator/InitiatorButtons.java
+++ src/client/crDetails/initiator/InitiatorButtons.java
@@ -1,7 +1,6 @@
 package client.crDetails.initiator;
 
 import client.ClientUI;
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import server.ServerService;
@@ -11,8 +10,6 @@ public class InitiatorButtons implements ClientUI {
     @FXML
     private Button attachFilesButton;
 
-    @FXML
-    private FontAwesomeIconView attachFile;
 
     @Override
     public void handleMessageFromClientController(ServerService serverService) {
