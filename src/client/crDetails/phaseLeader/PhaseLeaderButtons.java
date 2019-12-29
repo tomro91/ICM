ca87f8a74f5f2bc4c22ddc322debf7070c2324f0
morskurka <57572844+morskurka@users.noreply.github.com>
2019-12-29 12:33:53 +0200

    press "time extension request" -event
    press "time extension request" only  if the conditions  are correct, otherwise enter the popup window - Apply for extension of time Will not open

--- src/client/crDetails/phaseLeader/PhaseLeaderButtons.java
+++ src/client/crDetails/phaseLeader/PhaseLeaderButtons.java
@@ -1,5 +1,8 @@
 package client.crDetails.phaseLeader;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +11,7 @@ import client.ClientUI;
 import common.IcmUtils;
 import entities.ChangeInitiator;
 import entities.ChangeRequest;
+import entities.Phase;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -18,21 +22,79 @@ import client.crDetails.CrDetails;
 
 public class PhaseLeaderButtons implements ClientUI {
 
+	private ChangeRequest currRequest;
+	private static Phase currPhase;
+	private ClientController clientController;
+	
+	public void initialize() {
+		 try {
+	            clientController = ClientController.getInstance(this);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+
+	        List<ChangeRequest> params = new ArrayList<>();
+	        currRequest= CrDetails.getCurrRequest();
+	        params.add(currRequest);
+	        System.out.println(params);
+
+	        ServerService loadPhaseData = new ServerService(ServerService.DatabaseService.Get_Phase_Details, params);
+	        System.out.println(loadPhaseData);
+	        clientController.handleMessageFromClientUI(loadPhaseData);
+	       
+	}
  
     @FXML
     void showExtensionTimeDialog(ActionEvent event) {
     
-    	 try {
+    	LocalDate currDate = LocalDate.now();     // Create a date object
+    	LocalDate deadLine = currPhase.getDeadLine();
+    	final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    	final long  days = (ChronoUnit.DAYS.between(currDate, deadLine))+1;
+        System.out.println("Days between: " + days);
+    	
+    if(!(days< 4 && days > 0)|| days>3) {
+    	 IcmUtils.displayErrorMsg("ERROR","Current stage: " + currPhase.getName().toString() + "\n" +
+       	    	"Deadline: " + deadLine.format(formatter) + "\n" + "Time left: " + days +" days" + "\n","ERROR :This request can only be submitted if 3 or less days are left to complete this phase!" );	
+    	 //IcmUtils.getPopUp().close();
+    }
+   
+    else if(currPhase.isExtensionRequest()== true) {
+    	   IcmUtils.displayErrorMsg("ERROR","ERROR","A time extension request for this phase has already been submitted." +"\n" + "extension request can only be submitted once!" );
+    	   //IcmUtils.getPopUp().close();
+    	   }
+    	   
+    	
+    else {
+    	try {
     		 IcmUtils.popUpScene(this, "Extention Time Request", "/client/crDetails/phaseLeader/RequestExtensionTime/RequestExtensionTime.fxml",407 ,381 );
          } catch (IOException e) {
              e.printStackTrace();
          }
     }
+    	 
+    }
+    
 
     @Override
     public void handleMessageFromClientController(ServerService serverService) {
+final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     	
- 
+    	switch (serverService.getDatabaseService()) {
+
+        case Get_Phase_Details:
+        	List<Phase> phaseList= serverService.getParams();
+        	currPhase= phaseList.get(0);
+            break;
 }
-    
+    }    
+
+public static void setPhase (Phase NewPhase) {
+	currPhase=NewPhase;
+}
+
+public static Phase getPhase () {
+	return currPhase;
+}
+
 }
--- src/client/crDetails/phaseLeader/RequestExtensionTime/RequestExtensionTime.java
+++ src/client/crDetails/phaseLeader/RequestExtensionTime/RequestExtensionTime.java
@@ -9,6 +9,7 @@ import java.util.List;
 import client.ClientController;
 import client.ClientUI;
 import client.crDetails.CrDetails;
+import client.crDetails.phaseLeader.PhaseLeaderButtons;
 import common.IcmUtils;
 import entities.ChangeRequest;
 import entities.Phase;
@@ -35,12 +36,11 @@ public class RequestExtensionTime implements ClientUI{
 	@FXML
 	private Button CancelButton;
 
-	private ChangeRequest currRequest;
-	private Phase currPhase;
 	private Phase newCurrPhase;
 	private LocalDate datePickerChoice;
 	private String description;
 	private ClientController clientController;
+	final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	
 	public void initialize() {
 		 try {
@@ -48,41 +48,13 @@ public class RequestExtensionTime implements ClientUI{
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        }
-
-	        List<ChangeRequest> params = new ArrayList<>();
-	        currRequest= CrDetails.getCurrRequest();
-	        params.add(currRequest);
-	        System.out.println(params);
-
-	        ServerService loadPhaseData = new ServerService(ServerService.DatabaseService.Get_Phase_Details, params);
-	        System.out.println(loadPhaseData);
-	        clientController.handleMessageFromClientUI(loadPhaseData);
-	       
+  
 	}
 	
-
     @FXML
     void submitRequestTime(ActionEvent event) {
     	
-    	LocalDate currDate = LocalDate.now();     // Create a date object
-    	LocalDate deadLine = currPhase.getDeadLine();
-    	final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-    	final long  days = (ChronoUnit.DAYS.between(currDate, deadLine))+1;
-        System.out.println("Days between: " + days);
-    	
-    if(!(days< 4 && days > 0)|| days>3) {
-    	 IcmUtils.displayErrorMsg("ERROR","Current stage: " + currPhase.getName().toString() + "\n" +
-       	    	"Deadline: " + deadLine.format(formatter) + "\n" + "Time left: " + days +" days" + "\n","ERROR :This request can only be submitted if 3 or less days are left to complete this phase!" );	
-    	 IcmUtils.getPopUp().close();
-    }
-   
-    else if(currPhase.isExtensionRequest()== true) {
-    	   IcmUtils.displayErrorMsg("ERROR","ERROR","A time extension request for this step has already been submitted." +"\n" + "extension request can only be submitted once!" );
-    	   IcmUtils.getPopUp().close();
-    	   }
-    	   
-    	
-    	else {
+    	    newCurrPhase=PhaseLeaderButtons.getPhase();
     		datePickerChoice = RequestedtimeDatePicker.getValue();
     		description = DescriptionTextAreaDescriptionTextArea.getText();
     		System.out.println(description+datePickerChoice.format(formatter));
@@ -97,11 +69,9 @@ public class RequestExtensionTime implements ClientUI{
             System.out.printf("%s\n",phaseList);
             ServerService updatePhaseExtension = new ServerService(ServerService.DatabaseService.Update_Phase_Extension, phaseList);
             System.out.println(updatePhaseExtension);
-            clientController.handleMessageFromClientUI(updatePhaseExtension);
-            }
-    		
-          
+            clientController.handleMessageFromClientUI(updatePhaseExtension);  
     }
+    
     
     @FXML
     void cancelRequestTime(ActionEvent event) {
@@ -111,20 +81,14 @@ public class RequestExtensionTime implements ClientUI{
     @Override
     public void handleMessageFromClientController(ServerService serverService) {
      
-    	final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-    	
+    
     	switch (serverService.getDatabaseService()) {
 
-        case Get_Phase_Details:
-        	List<Phase> phaseList= serverService.getParams();
-        	currPhase= phaseList.get(0);
-        	newCurrPhase=phaseList.get(0);
-            break;
         case Update_Phase_Extension:
         	List<Boolean> update=serverService.getParams();
         	boolean checkUpdate= update.get(0);
         	if(checkUpdate== true) {
-        		IcmUtils.displayInformationMsg("Time Extension Request Submited", "Time extension request has been successfully submited", "Time extension request: " + currPhase.getExceptionTime().format(formatter));
+        		IcmUtils.displayInformationMsg("Time Extension Request Submited", "Time extension request has been successfully submited", "Time extension request: " + newCurrPhase.getExceptionTime().format(formatter));
         	    IcmUtils.getPopUp().close();
         	}
         	
